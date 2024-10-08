39666bc4de9efa4eef25bbabde7e08ba8f38e46e
Lương Phạm Việt Anh <108165794+vanhh59@users.noreply.github.com>
2024-10-08 20:55:49 +0700

    Update authMiddleware.js

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -6,32 +6,53 @@ const authenticate = asyncHandler(async (req, res, next) => {
   let token;
 
   // Read JWT from the 'jwt' cookie
-  if (req.cookies.jwt) {
+  token = req.cookies.jwt;
+
+  if (token) {
     try {
-      token = req.cookies.jwt;
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-      // Attach the user to the request object
       req.user = await User.findById(decoded.userId).select("-password");
-
       next();
     } catch (error) {
-      res.status(401).json({ message: "Not authorized, token failed" });
+      res.status(401);
+      throw new Error("Not authorized, token failed.");
     }
-  }
-
-  if (!token) {
-    res.status(401).json({ message: "Not authorized, no token" });
+  } else {
+    res.status(401);
+    throw new Error("Not authorized, no token.");
   }
 });
 
-// Middleware to check if the user is an admin
 const authorizeAdmin = (req, res, next) => {
   if (req.user && req.user.isAdmin) {
     next();
   } else {
-    res.status(403).json({ message: "Not authorized as an admin" });
+    res.status(401).send("Not authorized as an admin.");
   }
 };
 
-export { authenticate, authorizeAdmin };
+const authorizeStaff = (req, res, next) => {
+  if (req.user && req.user.isStaff) {
+    next();
+  } else {
+    res.status(401).send("Not authorized as an staff.");
+  }
+};
+
+const authorizeManager = (req, res, next) => {
+  if (req.user && req.user.isManager) {
+    next();
+  } else {
+    res.status(401).send("Not authorized as an manager.");
+  }
+};
+
+const authorizeForManage = (req, res, next) => {
+  if (req.user && (req.user.isManager || req.user.isStaff || req.user.isAdmin)) {
+    next();
+  } else {
+    res.status(403).send("Access denied. Only manager or staff can perform this action.");
+  }
+}
+
+export { authenticate, authorizeAdmin, authorizeStaff, authorizeManager, authorizeForManage };
