1151cdaa1d239d7635f9d2e1f693260687f196e6
vanhh59 <vanhhh59@gmail.com>
2024-10-10 13:50:37 +0700

    fix .gitignore

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -10,7 +10,7 @@ const authenticate = asyncHandler(async (req, res, next) => {
 
   if (token) {
     try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const decoded = jwt.verify(token, process.env.JWT_SECRET); 
       req.user = await User.findById(decoded.userId).select("-password");
       next();
     } catch (error) {
